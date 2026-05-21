3a6e7ba7bdabe65edf222a3626c1ba6dfa8d86e2
kings <270382241+portraitsofkings@users.noreply.github.com>
2026-05-21 13:43:24 +0300

    feat(foundations): solve tenth task

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+function checkDivisible(dividend, divisor) {
+  return !(dividend % divisor)
+}
 
+const leapYears = function(year) {
+  if (!checkDivisible(year, 4)) { return false }
+  if (checkDivisible(year, 100)) {
+    if (checkDivisible(year, 400)) {
+      return true
+    }
+    return false
+  }
+  return true
 };
 
 // Do not edit below this line
